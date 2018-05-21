7de567eda40447d09f5e36db5aa695c08a1dfad8
Emre <tanriverdi.emre2@gmail.com>
2018-05-21 19:50:38 +0300

    Possibly final commit

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(wikiOriginalLink) {
          var newUrl = wikiOriginalLink.url.replace("wikipedia.org", "0wikipedia.org");
          return {redirectUrl: newUrl};
-    },{ urls: [ "*://*.wikipedia.org/*"],}, ["blocking"]
+    },{urls: [ "*://*.wikipedia.org/*"], types: ["main_frame", "sub_frame", "other"]}, ["blocking"]
 );
