74679c833b1c4acc311d120bd67cb5ee8f7de786
Emre <tanriverdi.emre2@gmail.com>
2019-05-19 10:27:45 +0300

    After a year, updated the URL with its new mirror.
    I wasn't gonna update this extension anymore but here it goes. ¯\_(ツ)_/¯
    
    Since 0Wikipedia will stop working soon, I changed it to 1Wikipedia, which will be the new destination URL.

--- README.md
+++ README.md
@@ -9,9 +9,3 @@
   * Click **Load Unpacked Extension** and open/select the folder.
   * Ta-da!
   <br/><br/>
-* Firefox Installation
-  * Clone or download a zip of the repository
-  * Go to `Add-Ons`
-  * *Drag & drop* the **wikizero-redirector.xpi** file.
-  * Ta-da!
-  <br/><br/><br/>
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(wikiOriginalLink) {
-         var newUrl = wikiOriginalLink.url.replace("wikipedia.org", "0wikipedia.org");
+         var newUrl = wikiOriginalLink.url.replace("wikipedia.org", "1wikipedia.org");
          return {redirectUrl: newUrl};
     },{urls: [ "*://*.wikipedia.org/*"], types: ["main_frame", "sub_frame", "other"]}, ["blocking"]
 );
